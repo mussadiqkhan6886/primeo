6be7f2d272f5b2f8647228ab631407ebcf2df668
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2026-04-10 18:01:26 +0500

    headr done

--- sections/Header.tsx
+++ sections/Header.tsx
@@ -1,39 +1,62 @@
+'use client';
+
 import { syne } from '@/libs/fonts'
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Header = () => {
     const nav = [
-        {name: "About" ,link:"#about"},
-        {name: "8-Layers" ,link:"#layers"},
-        {name: "How it works" ,link:"#work"},
-        {name: "Results" ,link:"#result"},
-        {name: "Pricing" ,link:"#pricing"},
+        { name: "About", link: "#about" },
+        { name: "8-Layers", link: "#layers" },
+        { name: "How it works", link: "#work" },
+        { name: "Results", link: "#result" },
+        { name: "Pricing", link: "#pricing" },
     ]
-  return (
-    <header className='w-full flex items-center justify-center  fixed mx-auto top-2'>
-        <div className=' text-black bg-gray-50 rounded-full p-3 backdrop-blur-md  border-white shadow-md text-sm max-w-180 flex justify-between items-center w-180 px-10 gap-10 '>
-            {/* logo */}
-            <div>
-                <h1 className={`${syne.className} font-semibold`}>PRIMEO</h1>
-            </div>
-            {/* Nav */}
-            <nav>
-                <ul className='flex gap-6 items-center'>
-                    {nav.map(item => (
-                        <li key={item.name}><Link href={item.link}>{item.name}</Link></li>
-                    ))}
-                </ul>
-            </nav>
-            {/* CTA */}
-            <div>
-                    <button className='border-dark hover:bg-gold transition-all duration-300 rounded-lg hover:text-white hover:border-transparent border p-2 py-1 cursor-pointer'>
+
+    const [scrolled, setScrolled] = useState(false)
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setScrolled(window.scrollY >= 10)
+        }
+
+        window.addEventListener("scroll", handleScroll)
+
+        return () => window.removeEventListener("scroll", handleScroll)
+    }, []) 
+
+    return (
+        <header className='w-full flex items-center justify-center z-50 fixed mx-auto top-2'>
+            <div className={`
+                ${scrolled ? "w-[90%] md:w-[800px] bg-white/20" : "w-[95%] md:w-[750px] bg-white/20"} 
+                transition-all duration-300 text-black rounded-full p-2 backdrop-blur-md 
+                border border-white/20 shadow-md uppercase text-[12px] flex justify-between items-center px-10 gap-10
+            `}>
+                {/* Logo */}
+                <div>
+                    <h1 className={`${syne.className} font-semibold tracking-tight`}>PRIMEO</h1>
+                </div>
+
+                {/* Nav */}
+                <nav className="hidden md:block">
+                    <ul className='flex gap-6 items-center'>
+                        {nav.map(item => (
+                            <li key={item.name} className="hover:opacity-60 transition-opacity">
+                                <Link href={item.link}>{item.name}</Link>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+
+                {/* CTA */}
+                <div>
+                    <button className='bg-gold transition-all duration-300 rounded-lg text-white  px-4 py-1.5 cursor-pointer'>
                         Book a Call
                     </button>
+                </div>
             </div>
-      </div>
-    </header>
-  )
+        </header>
+    )
 }
 
 export default Header
--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Hero = () => {
   return (
     <section>
-      
+      <video src="/hero-background.mp4" playsInline autoPlay muted loop />
     </section>
   )
 }
